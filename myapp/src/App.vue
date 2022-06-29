064a5634b698ef610698931cf9f4343a89839468
Rasa Stojanovic <stojanovicrasa4@gmail.com>
2022-06-30 01:20:23 +0200

    Added: NavBar and AboutView

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<div>
+  <NavigationBar></NavigationBar>
   <router-view/>
+</div>
 </template>
-
+<!--
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -14,17 +13,25 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
+-->
+<style>
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
+
+
+<script>
+import NavigationBar from './components/NavigationBar.vue'
+
+export default{
+  name : 'App',
+  components:{
+    NavigationBar
+  }
+}
+</script>
